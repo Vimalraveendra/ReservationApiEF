b1efbb989b754eedd3492036fca11ef6d9766a49
Vimal Raveendran <vimalraveendra@gmail.com>
2021-08-16 23:20:39 +0200

    inital commit, added Guest & Reservation Models, also added the EntityFramework packages

---
+++ ReservationRestApi/Model/Guest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReservationRestApi.Model
+{
+    public class Guest
+    {
+        [Key]
+        [Required]
+       public int GuestId { get; set; }
+
+        [Required]
+       public string Name { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Column(TypeName = "Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? DOB { get; set; }
+
+        public double? PostalCode { get; set; }
+
+        public Reservation Reservation { get; set; }
+
+        [ForeignKey("ReservationId")]
+        public int ReservationId { get; set; }
+     
+    }
+}
---
+++ ReservationRestApi/Model/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReservationRestApi.Model
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        [Required,MaxLength(10)]
+        public  string ReservationCode { get; set; }
+
+        [Column(TypeName = "Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CreationDate { get; set; }
+
+        public double Price { get; set; }
+
+        [Column(TypeName = "Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CheckInDate { get; set; }
+
+        [Column(TypeName = "Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CheckOutDate { get; set; }
+
+        public string Currency { get; set; }
+
+        public decimal? commission { get; set; }
+        
+      
+        
+    }
+}
